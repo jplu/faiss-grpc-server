a9d6632a31328cd345cb00d78e00da20f799f51b
Julien Plu <plu.julien@gmail.com>
2021-08-02 12:35:15 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ DEFINE_string(port, "8080", "port of faiss server");
 DEFINE_string(file_path, "", "index file name");
 DEFINE_uint64(top_k, 10, "default number of neighbor");
 DEFINE_bool(on_cpu, false, "run on CPU or not");
+DEFINE_uint64(nprobe, 256, "nprobe > 0 && nprobe <= nlist");
 
 int main(int argc, char* argv[]) {
   const grpc::string kHealthyService("healthy_service");
@@ -19,7 +20,7 @@ int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   std::string address = FLAGS_host + ":" + FLAGS_port;
   auto logger = spdlog::stdout_color_mt("console");
-  FaissServer fsrv(logger, FLAGS_top_k, FLAGS_file_path.c_str(), FLAGS_on_cpu);
+  FaissServer fsrv(logger, FLAGS_top_k, FLAGS_file_path.c_str(), FLAGS_on_cpu, FLAGS_nprobe);
   grpc::ServerBuilder builder;
   builder.AddListeningPort(address, grpc::InsecureServerCredentials());
   builder.RegisterService(&fsrv);
